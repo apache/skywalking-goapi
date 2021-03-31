7b9b8428853887cc6393de97500f443194476ea6
Zhenxu Ke <kezhenxu94@apache.org>
2021-03-31 16:36:28 +0800

    Add scripts to update and commit the first version (#1)

---
+++ README.md
@@ -0,0 +1,5 @@
+# SkyWalking Go APIs
+
+This repository contains the Go files generated from
+the [data collect protocol](http://github.com/apache/skywalking-data-collect-protocol) and
+the [query protocol](http://github.com/apache/skywalking-query-protocol) for convenient use.
---
+++ dependencies.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+export COLLECT_PROTOCOL_SHA=c01af3b883217939e374d2245eac92036fb7394a
+export QUERY_PROTOCOL_SHA=42d6783d8c1a0d0d4c2b28e58dc1f47dd554964e
