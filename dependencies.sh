561b14f437f731109f1389b31ae86f3dcbeb0901
mrproliu <741550557@qq.com>
2021-07-12 14:32:36 +0800

    update collect protocol to support the JVM params (#10)
    * update collect protocol to support JVM params
    
    Co-authored-by: Mrproliu <mrproliu@lagou.com>

--- dependencies.sh
+++ dependencies.sh
@@ -17,5 +17,5 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export COLLECT_PROTOCOL_SHA=743f33119dc5621ae98b596eb8b131dd443445c7
+export COLLECT_PROTOCOL_SHA=e626ee04850703c220f64b642d2893fa65572943
 export QUERY_PROTOCOL_SHA=42d6783d8c1a0d0d4c2b28e58dc1f47dd554964e
