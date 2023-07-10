1fc7c0d84209a2efea0fe2d7394e4e1a69431f95
Mrproliu <741550557@qq.com>
2023-07-10 21:05:47 +0800

    Sync query protocol for dynamic menu document link field

--- query/schema.go
+++ query/schema.go
@@ -465,13 +465,13 @@ type MQEValues struct {
 }
 
 type MenuItem struct {
-	Title       string      `json:"title"`
-	Icon        *string     `json:"icon,omitempty"`
-	Layer       string      `json:"layer"`
-	Activate    bool        `json:"activate"`
-	SubItems    []*MenuItem `json:"subItems"`
-	Description *string     `json:"description,omitempty"`
-	Link        *string     `json:"link,omitempty"`
+	Title        string      `json:"title"`
+	Icon         *string     `json:"icon,omitempty"`
+	Layer        string      `json:"layer"`
+	Activate     bool        `json:"activate"`
+	SubItems     []*MenuItem `json:"subItems"`
+	Description  *string     `json:"description,omitempty"`
+	DocumentLink *string     `json:"documentLink,omitempty"`
 }
 
 type Metadata struct {
